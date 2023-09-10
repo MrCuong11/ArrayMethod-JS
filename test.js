f01d84b1566838c025b3bd5ddfb830b0610ea0e8
testGit <nmanhcuong.cpf@gmail.com>
2023-09-10 22:47:32 +0700

    hasOwnProperty method

--- test.js
+++ test.js
@@ -1,12 +1,22 @@
-//forEach 1: lặp qua các phần tử trong mảng
+
+// Phương thức hasOwnProperty trả về một giá trị boolean:
+
+// true nếu đối tượng có chứa thuộc tính được chỉ định.
+// false nếu đối tượng không có thuộc tính được chỉ định hoặc 
+// thuộc tính được chỉ định là một thuộc tính kế thừa từ prototype của đối tượng.
+
+
+//forEach 1: lặp qua các phần tử trong mảng, không có return 
 var courses = [
     'javascript',
     'python',
     'Ruby'
 ]
 Array.prototype.forEach1 = function (callback) {
-    for (let i = 0; i < this.length; i++) {
-        callback(this[i], i, this);
+    for (const key in this) {
+        if (this.hasOwnProperty(key)){
+            callback(this[key], key, this);
+        }
     }
 }
 
@@ -17,7 +27,7 @@ courses.forEach1((item) => {
 
 
 
-//my Find: trả về phần tử đầu tiên thỏa mãn điều kiện
+//my Find: trả về phần tử đầu tiên thỏa mãn điều kiện có return 
 Array.prototype.myFind = function (callback) {
     for (let i = 0; i < this.length; i++) {
         if (callback(this[i], i, this)) {
@@ -37,17 +47,20 @@ console.log(result);
 
 
 
-//filter1: trả về mảng gồm các phần tử thỏa mãn điều kiện 
+//filter1: trả về mảng gồm các phần tử thỏa mãn điều kiện, có return 
 
-Array.prototype.filter1 = function (callback) {
-    let newArray = [];
-    for (let i = 0; i < this.length; i++) {
-        // hàm callback trả về true tức là thỏa mãn điều kiện bên dưới (num % 2 == 0)
-        if (callback(this[i], i, this) === true) {
-            newArray.push(this[i]);
+Array.prototype.myFilter = function(cb) {
+    var output = [];
+    for( var index in this){
+        if(this.hasOwnProperty(index)){
+            var result = cb(this[index], index, this);
+            // result trả về true tức là thỏa mãn điều kiện bên ví dụ dưới
+            if(result){
+                output.push(this[index]);
+            }
         }
     }
-    return newArray;
+    return output;
 }
 var Arrays = [1, 2, 3, 4, 5, 6, 7, 8];
 
@@ -56,6 +69,7 @@ var result = Arrays.filter1(num => {
     return num % 2 == 0;
 });
 console.log(result.join(', '));
+
 
 
 
@@ -85,12 +99,14 @@ console.log(htmls.join(', '));
 
 
 // some: chỉ một điều kiện thỏa mãn => true
-Array.prototype.some1 = function (callback) {
-    for (let i = 0; i < this.length; i++) {
-        if (callback(this[i], i, this)) {
-            return true;
+Array.prototype.mySome = function(cb) {
+    for(var index in this){
+        if(this.hasOwnProperty(index)){
+            if(cb(this[index], index, this)){
+                return true;
+            }
         }
-    }
+    } 
     return false;
 }
 
@@ -118,11 +134,18 @@ var result = courses.some1((course) => {
 console.log(result);
 
 
+
+
+
+
+
 //every: chỉ một điều kiện không thỏa mãn => false
 Array.prototype.every2 = function (callback) {
-    for (let i = 0; i < this.length; i++) {
-        if (!callback(this[i], i, this)) {
-            return false;
+    for (var index in this) {
+        if (this.hasOwnProperty(index)) {
+            if (!callback(this[index], index, this)) {
+                return false;
+            }
         }
     }
     return true;
@@ -151,6 +174,8 @@ var result = courses.every2((course) => {
     return course.coin == 0;
 });
 console.log(result);
+
+
 
 
 
